de3e8d8c643f4465bc8c56aca82e7b221c02492f
administrator <nikolaev@ttcsoft.ru>
2016-07-20 02:25:36 +0300

    add blog page from db

---
+++ src/Blogger/BlogBundle/Controller/BlogController.php
@@ -0,0 +1,35 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Blogger\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Blog controller.
+ */
+class BlogController extends Controller
+{
+    /**
+     * Show a blog entry
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $blog = $em->getRepository('BloggerBlogBundle:Blog')->find($id);
+
+        if (!$blog) {
+            throw $this->createNotFoundException('Unable to find Blog post.');
+        }
+
+        return $this->render('BloggerBlogBundle:Blog:show.html.twig', array(
+            'blog'      => $blog,
+        ));
+    }
+}
